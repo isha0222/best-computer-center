453e0545935a41143dce54eb88cbf21b0784ff89
isha0222 <ishasanghavi2002@gmail.com>
2026-07-13 11:33:10 +0530

    Update server.ts

--- server.ts
+++ server.ts
@@ -47,7 +47,7 @@ app.use(cors());
 app.use(express.json());
   
   // Parse incoming JSON payloads
-  app.use(express.json());
+  // app.use(express.json());
 
   // -------------------------------------------------------------
   // API Endpoints
