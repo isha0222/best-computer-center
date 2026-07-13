8d36c097673c89d85aee08e7f37b6aeec8814f0d
isha0222 <ishasanghavi2002@gmail.com>
2026-07-13 10:53:16 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { defineConfig } from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/best-computer-center/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
